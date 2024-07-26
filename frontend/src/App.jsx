25248d5a9d110e3fb8a827d4741e006deaed36c1
hevardhan <paatukkorupulavan@gmail.com>
2024-07-27 00:24:42 +0530

    merge conflict solved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-import axios from "axios";
-import React, { useState, useEffect } from "react";
-import Navbar from "./components/Navbar";
-import Mesh from "./components/mesh";
-import Home from "./components/home";
-import Grobj from "./components/grobj";
-import Grobj2 from "./components/grobj2";
+import axios from 'axios'
+import React from 'react';
+import Navbar from './components/Navbar'
+import Mesh from './components/mesh'
+import Home from './components/home'
+import Grobj from './components/grobj'
+import Grobj2 from './components/grobj2'
+import Search from './components/Search'
+import AboutBody from './components/AboutBody'
 import Contact from "./components/ContactUsSection";
 import Members from "./components/Members";
 import EventsTitle from "./components/EventsTitle";
@@ -35,16 +37,42 @@ function App() {
           <Mesh />
         </div>
       </section>
-      <section>
-        <h2 class="text-white text-6xl text-center pt-14">About Us</h2>
-        <p class="text-white text-2xl text-center pt-2" >Google Developer Student Clubs (GDSC) are community groups for college and university students interested in Google developer technologies.</p>
+      <section className='sizeUp'>
+        <Search/>
+        <AboutBody/>
+        <button className='btn font-michrome ml'style={{ marginLeft: '36px' }}>MORE</button>
+      </section>
+      <section className="relative flex items-center justify-center" id='team'>
+        <div className="absolute inset-0 flex items-center justify-center">
+          <h1 className="font-michrome text-white text-9xl">MEET THE TEAM</h1>
+        </div>
+        <div className="relative h-full w-full  card-members">
+          <div className="flex items-center w-full justify-end gap-60 h-full">
+            <Members />
+            <Members />
+            <Members />
+            <Members />
+          </div>
+        </div>
+      </section>
+      <section id='events'>
+        <div>
+          <EventsTitle />
+        </div>
+        <div className="h-3/4 items-center flex justify-center">
+        <div className="events-top">
+          <a href="youtube.com">
+          <EventsCard />
+          </a>
+        </div>
+        </div>
       </section>
       <section id='join' className="justify-center items-center flex bg-red-500">
         <JoinContainer />
       </section>
       <Contact/>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
