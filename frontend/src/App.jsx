e6f2071407452378551b2bff9f795bdded5f7d9c
hevardhan <paatukkorupulavan@gmail.com>
2024-07-26 22:00:12 +0530

    team and events

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,24 @@
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-import Navbar from './components/Navbar';
-import Mesh from './components/mesh';
-import Home from './components/home';
-import Grobj from './components/grobj';
-import Grobj2 from './components/grobj2';
-
-import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Mesh from "./components/mesh";
+import Home from "./components/home";
+import Grobj from "./components/grobj";
+import Grobj2 from "./components/grobj2";
+import Contact from "./components/ContactUsSection";
+import Members from "./components/Members";
+import EventsTitle from "./components/EventsTitle";
+import EventsCard from "./components/EventsCard";
+import "./App.css";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 function App() {
   return (
     <>
       <section>
-          <Navbar />
+        <Navbar />
         <div className="h-full flex flex-col">
-          <div className="flex-1 animation_layer" >
+          <div className="flex-1 animation_layer">
             <Home />
             <div className="bottom-0 left-0 right-0 flex justify-between w-full">
               <Grobj />
@@ -31,12 +35,37 @@ function App() {
       </section>
       <section>
         <h2 class="text-white text-6xl text-center pt-14">About Us</h2>
-        <p class="text-white text-2xl text-center pt-2" >Google Developer Student Clubs (GDSC) are community groups for college and university students interested in Google developer technologies.</p>
+        <p class="text-white text-2xl text-center pt-2">
+          Google Developer Student Clubs (GDSC) are community groups for college
+          and university students interested in Google developer technologies.
+        </p>
       </section>
-      <section className="team">
-        <h1 className="font-michrome">MEET THE TEAM</h1>
+      <section className="relative flex items-center justify-center">
+        <div className="absolute inset-0 flex items-center justify-center">
+          <h1 className="font-michrome text-white text-9xl">MEET THE TEAM</h1>
+        </div>
+        <div className="relative h-full w-full  card-members">
+          <div className="flex items-center w-full justify-end gap-60 h-full">
+            <Members />
+            <Members />
+            <Members />
+            <Members />
+          </div>
+        </div>
       </section>
-      
+      <section className="">
+        <div>
+          <EventsTitle />
+        </div>
+        <div className="h-3/4 items-center flex justify-center">
+        <div className="events-top">
+          <a href="youtube.com">
+          <EventsCard />
+          </a>
+        </div>
+        </div>
+      </section>
+      <Contact />
     </>
   );
 }
---
+++ frontend/src/components/EventsTitle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EventsTitle = () => {
+  return (
+    <div className='w-full'>
+        <h1 className='text-white text-center text-9xl'>Events</h1>
+    </div>
+  )
+}
+
+export default EventsTitle
