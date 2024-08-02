dc6e72a3e5b87c4f7293cf3b4472e9c5f00d9936
Faheem <faheemuddinsayyed789@gmail.com>
2024-08-02 12:01:38 +0530

    Added links

--- frontend/src/components/SocialLinks.jsx
+++ frontend/src/components/SocialLinks.jsx
@@ -5,22 +5,30 @@ const SocialLinks = () => {
     <div className="parent">
       <div className="child child-1">
         <button className="SocialButton btn-1">
-          <img src="/facebook.png" alt="Facebook" height="32" className='facebookSmall'/>
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <img src="/facebook.png" alt="Facebook" height="32" className='facebookSmall'/>
+          </a>
         </button>
       </div>
       <div className="child child-2">
         <button className="SocialButton btn-2">
-          <img src="/instagram.png" alt="Instagram" height="32" />
+          <a href="https://www.instagram.com/gdsc_sit/" target="_blank" rel="noopener noreferrer">
+            <img src="/instagram.png" alt="Instagram" height="32" />
+          </a>
         </button>
       </div>
       <div className="child child-3">
         <button className="SocialButton btn-3">
-          <img src="/twitter.png" alt="Twitter" height="32" />
+          <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+            <img src="/twitter.png" alt="Twitter" height="32" />
+          </a>
         </button>
       </div>
       <div className="child child-4">
         <button className="SocialButton btn-4">
-          <img src="/linkedin.png" alt="LinkedIn" height="32" />
+          <a href="https://www.linkedin.com/company/gdsc-sit-pune/" target="_blank" rel="noopener noreferrer">
+            <img src="/linkedin.png" alt="LinkedIn" height="32" />
+          </a>
         </button>
       </div>
     </div>
