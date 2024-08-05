6bd2a4de7ece247d80e8632a14044708d8434b7d
hevardhan <paatukkorupulavan@gmail.com>
2024-08-05 14:06:25 +0530

    models updated

--- backend/models.py
+++ backend/models.py
@@ -5,7 +5,7 @@ class React(models.Model):
     Event_Name = models.CharField(max_length=30, null=True, blank=True)
     Form_Link    = models.CharField(max_length=30, null=True, blank=True)
     Date        = models.DateField(null=True, blank=True)
-    Poster = models.ImageField(upload_to='photos/', null=True, blank=True)
+    Poster = models.ImageField(upload_to='events/', null=True, blank=True)
 
 class Team(models.Model):
     Name = models.CharField(max_length=30, null=True, blank=True)
